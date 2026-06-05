cdd85af8381ba3cd1bd6e155c2026287abd6d776
Cirious Studio <cirious.studio@gmail.com>
2026-06-05 03:40:08 -0300

    feat: implement rich terminal styling via integration with cirious-codex-term

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,10 @@ pub mod dispatcher;
 pub mod format;
 pub mod level;
 pub mod log;
+pub mod style;
 
 pub use dispatcher::{Dispatcher, StderrDispatcher, StdoutDispatcher};
 pub use format::{Formatter, HumanReadableFormatter, JsonFormatter, Record};
 pub use level::Level;
 pub use log::*;
+pub use style::{level_color, StyledTerminalFormatter};
